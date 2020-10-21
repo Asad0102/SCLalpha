ae67d626ff19895e8d15f6528c54cd881cca668e
Michail Isaakidis <michaili@axis.com>
2020-10-21 15:13:24 +0200

    Test commit
    Change-Id: I2b70cd1369846fc06cc74cb17437d67fdea46da7

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 """
 Script for util functions
 """
+
 import os
 
 
@@ -8,5 +9,3 @@ def create_folder(folder_name):
     if not os.path.exists(folder_name):
         print('{} folder does not exist, creating it.'.format(folder_name))
         os.makedirs(folder_name)
-
-
